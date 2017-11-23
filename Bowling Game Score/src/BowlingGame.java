58383fd785c02eb861d5dfc8ac870ed77aac38fd
AndreasLjungberg <30982411+AndreasLjungberg@users.noreply.github.com>
2017-11-23 13:54:02 +0100

    Inital commit
    day 1

---
+++ Bowling Game Score/src/BowlingGame.java
@@ -0,0 +1,30 @@
+/** BowlingGame Score calculator 
+ *
+ * @author CDT414 alg11009 - Andreas Ljungberg: 
+ * @version 1.0 
+ * @date 2017-11-23
+ * 
+ * Version	Initials	Time	Description
+ * 
+ */
+public class BowlingGame {
+
+	/** BowlingGame Score calculator constructor which require string as input 
+	 * @param game Expected format "[n,n][n,n]..[n,n]"
+	 * 
+	 */	 
+	public BowlingGame(String game)
+	{	
+		//TODO: create constructor for BowlingGame
+	}
+	
+	/** getScore method returns a score of current Bowling game or -1 if error
+	 * 
+	 * @return Integer value of Bowling score, in case of error return value is -1 
+	 */
+	public int getScore() {
+		//TODO: calculate the score of game and return correct value
+		return(-1);
+	}
+	
+}
